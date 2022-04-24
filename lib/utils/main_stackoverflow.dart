d935eb3ef57e67d2d6aa62a8e0b16bf176ed4f0e
Archanciel <jp.schnyder@gmail.com>
2022-04-24 06:02:49 +0200

    Create main_stackoverflow.dart

---
+++ lib/utils/main_stackoverflow.dart
@@ -0,0 +1,10 @@
+void main() {
+  print(Duration(hours: -1)); // -1:00:00.000000
+  print(Duration.zero - Duration(hours: 1)); // -1:00:00.000000
+  print(Duration(minutes: -1)); // 0:01:00.000000 instead of -0:01:00.000000
+  print(Duration.zero -
+      Duration(minutes: 1)); // 0:01:00.000000 instead of -0:01:00.000000
+
+  print(Duration(minutes: -1).inMinutes); // -1
+  print((Duration.zero - Duration(minutes: 1)).inMinutes); // -1
+}
