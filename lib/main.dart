7aed132019ab501b185344e67145e828752c2bd4
Archanciel <jp.schnyder@gmail.com>
2022-04-26 19:50:58 +0200

    can now be launched

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 import 'package:date_time_picker/date_time_picker.dart';
 
 import 'package:circa_plan/utils/date_time_parser.dart';
+import 'package:intl/intl.dart';
+
+
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() => runApp(const MyApp());
 
@@ -14,15 +18,13 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       title: appTitle,
-      home: Scaffold(
-        backgroundColor: Colors.blue,
-        appBar: AppBar(
-          backgroundColor: Colors.blue[900],
-          foregroundColor: Colors.yellow[300],
-          title: const Text(appTitle),
-        ),
-        body: const MyCustomForm(),
-      ),
+      home: MyCustomForm(),
+      localizationsDelegates: [
+        GlobalWidgetsLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: [Locale('fr', 'CH')], //, Locale('pt', 'BR')],
     );
   }
 }
@@ -42,10 +44,52 @@ class MyCustomForm extends StatefulWidget {
 class MyCustomFormState extends State<MyCustomForm> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
+  late TextEditingController _controller1;
+  late TextEditingController _controller2;
+  late TextEditingController _controller3;
+  late TextEditingController _controller4;
+
+
+  late DateFormat _dateTimeFormat;
+  late DateFormat _dateOnlyFormat;
+
+  //String _initialValue = '';
+  String _valueChanged1 = '';
+  String _valueToValidate1 = '';
+  String _valueSaved1 = '';
+  String _valueChanged2 = '';
+  String _valueToValidate2 = '';
+  String _valueSaved2 = '';
+  String _valueChanged3 = '';
+  String _valueToValidate3 = '';
+  String _valueSaved3 = '';
+  String _valueChanged4 = '';
+  String _valueToValidate4 = '';
+  String _valueSaved4 = '';
+
   String? _wakeUpDT;
   String? _awakeHHmm;
   String? _goToBedDT;
   String? _outputText;
+
+  @override
+  void initState() {
+    super.initState();
+    final DateTime dateTimeNow = DateTime.now();
+    String _initialValue = dateTimeNow.toString();
+    final String localName = 'fr_CH';
+    Intl.defaultLocale = localName;
+    _dateTimeFormat = DateFormat("dd-MM-yyyy HH:mm");
+    _dateOnlyFormat = DateFormat("dd-MM-yyyy");
+
+    _controller1 = TextEditingController(text: _initialValue);
+    _controller2 = TextEditingController(text: _initialValue);
+    _controller3 = TextEditingController(text: _initialValue);
+
+    String lsHour = dateTimeNow.hour.toString().padLeft(2, '0');
+    String lsMinute = dateTimeNow.minute.toString().padLeft(2, '0');
+    _controller4 = TextEditingController(text: '$lsHour:$lsMinute');
+  }
 
   String? _validateDateTime(String? value) {
     if (value == null || value.isEmpty) {
@@ -73,18 +117,47 @@ class MyCustomFormState extends State<MyCustomForm> {
     return null;
   }
 
+  String _reformatDateTimeStr(String dateTimeStr) => (dateTimeStr != '')
+      ? _dateTimeFormat.format(DateTime.parse(dateTimeStr))
+      : '';
+  String _reformatDateStr(String dateTimeStr) => (dateTimeStr != '')
+      ? _dateOnlyFormat.format(DateTime.parse(dateTimeStr))
+      : '';
+
   @override
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
-    return Container(
-      margin: const EdgeInsets.all(10),
-      child: Form(
-        key: _formKey,
-        child: SingleChildScrollView(
+    return Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          backgroundColor: Colors.blue[900],
+          foregroundColor: Colors.yellow[300],
+          title: const Text('Circadian App'),
+        ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
+              DateTimePicker(
+                type: DateTimePickerType.dateTimeSeparate,
+                dateMask: 'dd MMM yyyy',
+                use24HourFormat: true,
+                controller: _controller1,
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2100),
+                icon: Icon(Icons.event),
+                dateLabelText: 'Date',
+                timeLabelText: "Hour",
+                onChanged: (val) => setState(() => _valueChanged1 = val),
+                validator: (val) {
+                  setState(() => _valueToValidate1 = val ?? '');
+                  return null;
+                },
+                onSaved: (val) => setState(() => _valueSaved1 = val ?? ''),
+              ),
               TextFormField(
                 style: TextStyle(
                   color: Colors.white,
