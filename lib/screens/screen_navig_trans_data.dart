7d57784e6ebae2ddf719bb5b792b13c64c9f114a
Archanciel <jp.schnyder@gmail.com>
2022-05-01 19:14:34 +0200

    Update screen_navig_trans_data.dart

--- lib/screens/screen_navig_trans_data.dart
+++ lib/screens/screen_navig_trans_data.dart
@@ -1,4 +1,7 @@
 class ScreenNavigTransData {
+  /// This class stores the screen data in order for the screen state
+  /// to be maintained when shifting to another screen and then coming
+  /// back to the previous screen.
   final Map<String, dynamic> _transferDataMap;
 
   const ScreenNavigTransData({required Map<String, dynamic> transferDataMap})
