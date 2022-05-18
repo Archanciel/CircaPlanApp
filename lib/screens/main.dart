bbc757cc8e89144374ba94e851cd88f230e94080
Archanciel <jp.schnyder@gmail.com>
2022-05-18 19:08:36 +0200

    Delete main.dart

--- lib/screens/main.dart
+++
@@ -1,8 +0,0 @@
-void main() {
-  final String one = '12-08-2022 13:34';
-  final String two = '12-08-2021 13:34';
-  final List lstOne = one.split('');
-  final List lstTwo = two.split('');
-
-  final List lstDiff =
-}
