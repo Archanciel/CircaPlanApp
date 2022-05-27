875990af75a2a38ccebd5b0905ace1ec34449909
Archanciel <jp.schnyder@gmail.com>
2022-05-28 00:55:31 +0200

    iproved Reset button location

--- lib/screens/calculate_sleep_duration.dart
+++ lib/screens/calculate_sleep_duration.dart
@@ -806,7 +806,7 @@ class _CalculateSleepDurationState extends State<CalculateSleepDuration>
         Align(
           alignment: Alignment.bottomLeft,
           child: Container(
-            margin: const EdgeInsets.fromLTRB(265, 0, 0, 78),
+            margin: const EdgeInsets.fromLTRB(265, 0, 0, 8),
             child: ElevatedButton(
               style: ButtonStyle(
                   backgroundColor: appElevatedButtonBackgroundColor,
