a4784fec87105e30ae4c4b4eebc510ed5edb04c5
Archanciel <jp.schnyder@gmail.com>
2022-07-14 16:56:51 +0200

    Update date_time_difference_duration_data.dart

--- lib/model/date_time_difference_duration_data.dart
+++ lib/model/date_time_difference_duration_data.dart
@@ -9,19 +9,39 @@ class DateTimeDifferenceDurationData extends ScreenData {
   DateTimeDifferenceDurationData() {
     screenDataType = ScreenDataType.dateTimeDifferenceDurationData;
   }
-  /*
-    map['dtDiffStartDateTimeStr'] = _startDateTimeStr;
-    map['dtDiffEndDateTimeStr'] = _endDateTimeStr;
-    map['dtDiffDurationStr'] = _durationStr;
-    map['dtDiffAddTimeStr'] = _addTimeStr;
-    map['dtDiffFinalDurationStr'] = _finalDurationStr;
-*/
-  String get addDurationStartDateTimeStr => attributes['addDurationStartDateTimeStr'];
-  set addDurationStartDateTimeStr(String value) => attributes['addDurationStartDateTimeStr'] = value;
 
-  String get durationStr => attributes['durationStr'];
-  set durationStr(String value) => attributes['durationStr'] = value;
+  /// Alternative to dart getter since testing if null is returned by
+  /// the method is useful.
+  ///
+  /// A getter can not return null. Null is returned if no
+  /// DateTimeDifferenceDuration screen field was modified.
+  String? getDateTimeDifferenceStartDateTimeStr() {
+    return attributes['dateTimeDifferenceStartDateTimeStr'];
+  }
 
-  String get endDateTimeStr => attributes['endDateTimeStr'];
-  set endDateTimeStr(String value) => attributes['endDateTimeStr'] = value;
+  String get dateTimeDifferenceStartDateTimeStr => attributes['dateTimeDifferenceStartDateTimeStr'];
+  set dateTimeDifferenceStartDateTimeStr(String value) => attributes['dateTimeDifferenceStartDateTimeStr'] = value;
+
+  String get dateTimeDifferenceEndDateTimeStr => attributes['dateTimeDifferenceEndDateTimeStr'];
+  set dateTimeDifferenceEndDateTimeStr(String value) => attributes['dateTimeDifferenceEndDateTimeStr'] = value;
+
+  String get dateTimeDifferenceDurationStr => attributes['dateTimeDifferenceDurationStr'];
+  set dateTimeDifferenceDurationStr(String value) => attributes['dateTimeDifferenceDurationStr'] = value;
+
+  String get dateTimeDifferenceAddTimeStr => attributes['dateTimeDifferenceAddTimeStr'];
+  set dateTimeDifferenceAddTimeStr(String value) => attributes['dateTimeDifferenceAddTimeStr'] = value;
+
+  String get dateTimeDifferenceFinalDurationStr => attributes['dateTimeDifferenceFinalDurationStr'];
+  set dateTimeDifferenceFinalDurationStr(String value) => attributes['dateTimeDifferenceFinalDurationStr'] = value;
+
+  @override
+  String toString() {
+    String? dateTimeDifferenceStartDateTimeStr = getDateTimeDifferenceStartDateTimeStr();
+
+    if (dateTimeDifferenceStartDateTimeStr == null) {
+      return '';
+    } else {
+      return 'dateTimeDifferenceStartDateTimeStr: $dateTimeDifferenceStartDateTimeStr\ndateTimeDifferenceEndDateTimeStr: $dateTimeDifferenceEndDateTimeStr\ndateTimeDifferenceDurationStr: $dateTimeDifferenceDurationStr\ndateTimeDifferenceAddTimeStr: $dateTimeDifferenceAddTimeStr\ndateTimeDifferenceFinalDurationStr: $dateTimeDifferenceFinalDurationStr';
+    }
+  }
 }
