fbc3f297012eae7e106c6da666c8036c2edc71d7
Archanciel <jp.schnyder@gmail.com>
2022-07-18 12:57:56 +0200

    added appbar menu

--- lib/main.dart
+++ lib/main.dart
@@ -190,6 +190,35 @@ class _MainAppState extends State<MainApp> with ScreenMixin {
                   ),
                   elevation: 0,
                   centerTitle: true,
+                  actions: [
+                    PopupMenuButton(
+                        // add icon, by default "3 dot" icon
+                        // icon: Icon(Icons.book)
+                        itemBuilder: (context) {
+                      return [
+                        PopupMenuItem<int>(
+                          value: 0,
+                          child: Text("My Account"),
+                        ),
+                        PopupMenuItem<int>(
+                          value: 1,
+                          child: Text("Settings"),
+                        ),
+                        PopupMenuItem<int>(
+                          value: 2,
+                          child: Text("Logout"),
+                        ),
+                      ];
+                    }, onSelected: (value) {
+                      if (value == 0) {
+                        print("My account menu is selected.");
+                      } else if (value == 1) {
+                        print("Settings menu is selected.");
+                      } else if (value == 2) {
+                        print("Logout menu is selected.");
+                      }
+                    }),
+                  ],
                 ),
               ),
               Positioned(
