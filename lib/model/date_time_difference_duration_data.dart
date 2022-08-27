7d8e0d1d59b079fbe65929e1d7af9d776f59c7c2
Archanciel <jp.schnyder@gmail.com>
2022-08-27 18:22:52 +0200

    now works except display update 2nd screen on load

--- lib/model/date_time_difference_duration_data.dart
+++ lib/model/date_time_difference_duration_data.dart
@@ -1,7 +1,7 @@
 import 'package:circa_plan/model/screen_data.dart';
 
 /// Add duration screen data class.
-/// 
+///
 /// Inherit from [ScreenData] base class. Its unique instance will be
 /// added to the [TransferData] instance which is responsible of saving
 /// and loading data to and from json file.
@@ -19,27 +19,50 @@ class DateTimeDifferenceDurationData extends ScreenData {
     return attributes['dateTimeDifferenceStartDateTimeStr'];
   }
 
-  String get dateTimeDifferenceStartDateTimeStr => attributes['dateTimeDifferenceStartDateTimeStr'];
-  set dateTimeDifferenceStartDateTimeStr(String value) => attributes['dateTimeDifferenceStartDateTimeStr'] = value;
+  String get dateTimeDifferenceStartDateTimeStr =>
+      attributes['dateTimeDifferenceStartDateTimeStr'];
+  set dateTimeDifferenceStartDateTimeStr(String value) =>
+      attributes['dateTimeDifferenceStartDateTimeStr'] = value;
 
-  String get dateTimeDifferenceEndDateTimeStr => attributes['dateTimeDifferenceEndDateTimeStr'];
-  set dateTimeDifferenceEndDateTimeStr(String value) => attributes['dateTimeDifferenceEndDateTimeStr'] = value;
+  String get dateTimeDifferenceEndDateTimeStr =>
+      attributes['dateTimeDifferenceEndDateTimeStr'];
+  set dateTimeDifferenceEndDateTimeStr(String value) =>
+      attributes['dateTimeDifferenceEndDateTimeStr'] = value;
 
-  String get dateTimeDifferenceDurationStr => attributes['dateTimeDifferenceDurationStr'];
-  set dateTimeDifferenceDurationStr(String value) => attributes['dateTimeDifferenceDurationStr'] = value;
+  String get dateTimeDifferenceDurationStr =>
+      attributes['dateTimeDifferenceDurationStr'];
+  set dateTimeDifferenceDurationStr(String value) =>
+      attributes['dateTimeDifferenceDurationStr'] = value;
 
-  String get dateTimeDifferenceAddTimeStr => attributes['dateTimeDifferenceAddTimeStr'];
-  set dateTimeDifferenceAddTimeStr(String value) => attributes['dateTimeDifferenceAddTimeStr'] = value;
+  String get dateTimeDifferenceAddTimeStr =>
+      attributes['dateTimeDifferenceAddTimeStr'];
+  set dateTimeDifferenceAddTimeStr(String value) =>
+      attributes['dateTimeDifferenceAddTimeStr'] = value;
 
-  String get dateTimeDifferenceFinalDurationStr => attributes['dateTimeDifferenceFinalDurationStr'];
-  set dateTimeDifferenceFinalDurationStr(String value) => attributes['dateTimeDifferenceFinalDurationStr'] = value;
+  String get dateTimeDifferenceFinalDurationStr =>
+      attributes['dateTimeDifferenceFinalDurationStr'];
+  set dateTimeDifferenceFinalDurationStr(String value) =>
+      attributes['dateTimeDifferenceFinalDurationStr'] = value;
 
-  String get dateTimeDurationPercentStr => attributes['dtDurationPercentStr'];
-  set dateTimeDurationPercentStr(String value) => attributes['dtDurationPercentStr'] = value;
+  /// Checking if the durationPercentStr == null solves the
+  /// problem of starting the app on a physical device where
+  /// the json files do not yet have the 
+  /// 'dateTimeDurationPercentStr' entry !
+  String get dateTimeDurationPercentStr {
+    String? durationPercentStr = attributes['dateTimeDurationPercentStr'];
+
+    durationPercentStr ??= '100 %';
+
+    return durationPercentStr;
+  }
+
+  set dateTimeDurationPercentStr(String value) =>
+      attributes['dateTimeDurationPercentStr'] = value;
 
   @override
   String toString() {
-    String? dateTimeDifferenceStartDateTimeStr = getDateTimeDifferenceStartDateTimeStr();
+    String? dateTimeDifferenceStartDateTimeStr =
+        getDateTimeDifferenceStartDateTimeStr();
 
     if (dateTimeDifferenceStartDateTimeStr == null) {
       return '';
