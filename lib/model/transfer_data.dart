dfc6f92c4b35ecf07f1216561eb4d9f91ab33bfb
Archanciel <jp.schnyder@gmail.com>
2022-09-05 10:32:51 +0200

    Update transfer_data.dart

--- lib/model/transfer_data.dart
+++ lib/model/transfer_data.dart
@@ -61,9 +61,8 @@ class TransferData extends SerializableObject {
   set timeCalculatorData(TimeCalculatorData value) =>
       attributes['timeCalculatorData'] = value;
 
-  Future<TransferData> loadTransferDataFromFile({
-    required String jsonFilePathName,
-  }) async {
+  Future<TransferData> loadTransferDataFromFile(
+      {required String jsonFilePathName}) async {
     final Serializer serializer = Serializer();
     final String inputJsonStr = await File(jsonFilePathName).readAsString();
 
@@ -81,11 +80,6 @@ class TransferData extends SerializableObject {
     // print(
     //     'loadTransferDataFromFile($jsonFilePathName)\nsleepDurationStr: ${calculateSleepDurationData.sleepDurationStr}\nsleepHistoryDateTimeStrLst: ${calculateSleepDurationData.sleepHistoryDateTimeStrLst}');
 
-    Utility.renameFile(
-      filePathNameStr: jsonFilePathName,
-      newFileNameStr: '$kDefaultJsonFileName-1',
-    );
-
     return deserializedTransferData;
   }
 
@@ -96,17 +90,18 @@ class TransferData extends SerializableObject {
     final bool jsonFileExist = await File(jsonFilePathName).exists();
 
     if (jsonFileExist && jsonUndoFileName != null) {
-      String circadianJsonFilePathName =
+      String jsonUndoFilePathName =
           '/storage/emulated/0/Download/CircadianData/$jsonUndoFileName';
+
       // String screenDataSubMapKey = 'dateTimeDifferenceDurationData';
       String screenDataSubMapKey = 'calculateSleepDurationData';
 
       String formattedScreenDataSubMap =
           await Utility.formatScreenDataSubMapFromJsonFileContent(
-              jsonFilePathName: circadianJsonFilePathName,
+              jsonFilePathName: jsonUndoFilePathName,
               screenDataSubMapKey: screenDataSubMapKey);
       print(
-          '\n$jsonUndoFileName CONTENT BEFORE RENAMING circadian.json$circadianJsonFilePathName\n$formattedScreenDataSubMap');
+          '\n$jsonUndoFileName CONTENT BEFORE RENAMING $kDefaultJsonFileName to $jsonUndoFileName\n$formattedScreenDataSubMap');
 
       Utility.renameFile(
         filePathNameStr: jsonFilePathName,
@@ -115,10 +110,10 @@ class TransferData extends SerializableObject {
 
       formattedScreenDataSubMap =
           await Utility.formatScreenDataSubMapFromJsonFileContent(
-              jsonFilePathName: circadianJsonFilePathName,
+              jsonFilePathName: jsonUndoFilePathName,
               screenDataSubMapKey: screenDataSubMapKey);
       print(
-          '\n$jsonUndoFileName CONTENT AFTER RENAMING circadian.json$circadianJsonFilePathName\n$formattedScreenDataSubMap');
+          '\n$jsonUndoFileName CONTENT AFTER RENAMING $kDefaultJsonFileName to $jsonUndoFileName\n$formattedScreenDataSubMap');
     }
 
     final Serializer serializer = Serializer();
@@ -136,34 +131,4 @@ class TransferData extends SerializableObject {
     // String circadianJsonFilePathName = '/storage/emulated/0/Download/CircadianData/circadian.json';
     // Utility.formatJsonFileContent(jsonFilePathName: circadianJsonFilePathName).then((value) => print('$circadianJsonFilePathName\n$value'));
   }
-}
-
-Future<void> main() async {
-  AddDurationToDateTimeData addDurationToDateTimeData =
-      AddDurationToDateTimeData();
-  addDurationToDateTimeData.firstDurationIconType = DurationIconType.add;
-  addDurationToDateTimeData.addDurationStartDateTimeStr = '09_07_2022 23:58';
-  addDurationToDateTimeData.firstAddDurationDurationStr = '01:00';
-  addDurationToDateTimeData.firstAddDurationEndDateTimeStr = '10_07_2022 00:58';
-
-  TransferData transferData = TransferData();
-  transferData.addDurationToDateTimeData = addDurationToDateTimeData;
-
-  String jsonFilePathName = 'transfer_data.json';
-  transferData.saveTransferDataToFile(
-      jsonFilePathName: jsonFilePathName,
-      jsonUndoFileName: 'transfer_data.json-1');
-
-  TransferData loadedTransferData = TransferData();
-  await loadedTransferData.loadTransferDataFromFile(
-      jsonFilePathName: jsonFilePathName);
-
-  AddDurationToDateTimeData loadedAddDurationToDateTimeData =
-      loadedTransferData.addDurationToDateTimeData;
-
-  print(loadedAddDurationToDateTimeData.screenDataType);
-  print(loadedAddDurationToDateTimeData.firstDurationIconType);
-  print(loadedAddDurationToDateTimeData.addDurationStartDateTimeStr);
-  print(loadedAddDurationToDateTimeData.firstAddDurationDurationStr);
-  print(loadedAddDurationToDateTimeData.firstAddDurationEndDateTimeStr);
 }
