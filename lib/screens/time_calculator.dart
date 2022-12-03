84e9f79293c8995ee5ca94b289224d50cd9c7391
Archanciel <jp.schnyder@gmail.com>
2022-12-03 15:29:26 +0100

    now undo works again. UI and all tsts ok.

--- lib/screens/time_calculator.dart
+++ lib/screens/time_calculator.dart
@@ -834,13 +834,9 @@ class _TimeCalculatorState extends State<TimeCalculator> with ScreenMixin {
                               _transferDataViewModel
                                   .updateAndSaveTransferData();
 
-                              // _updateTransferDataMap(); not solving undo bug
-
                               setState(() {
                                 _divideFirstBySecond = value!;
                               });
-
-                              _updateTransferDataMap();
                             },
                           ),
                         ),
