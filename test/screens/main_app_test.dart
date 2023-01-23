8f7f9a79227946818018903c3082183afa4b80be
Archanciel <jp.schnyder@gmail.com>
2023-01-23 18:16:11 +0100

    Create main_app_test.dart

---
+++ test/screens/main_app_test.dart
@@ -0,0 +1,209 @@
+import 'package:circa_plan/main.dart';
+import 'package:circa_plan/screens/calculate_sleep_duration.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'dart:io';
+
+import 'package:circa_plan/constants.dart';
+import 'package:circa_plan/buslog/transfer_data_view_model.dart';
+import 'package:circa_plan/screens/screen_navig_trans_data.dart';
+import 'package:circa_plan/screens/add_duration_to_datetime.dart';
+import 'package:circa_plan/utils/date_time_parser.dart';
+import 'package:circa_plan/widgets/duration_date_time_editor.dart';
+import 'package:circa_plan/widgets/editable_date_time.dart';
+
+Future<void> main() async {
+  Map<String, dynamic> transferDataMapOne = {
+    "firstDurationIconData": Icons.add,
+    "firstDurationIconColor": Colors.green.shade200,
+    "firstDurationSign": 1,
+    "firstDurationTextColor": Colors.green.shade200,
+    "addDurStartDateTimeStr": "2022-07-12 16:00",
+    "firstDurationStr": "00:50",
+    "firstStartDateTimeStr": "2022-07-12 16:00",
+    "firstEndDateTimeStr": "2022-07-12 16:50",
+    "firstEndDateTimeCheckbox": false,
+    "secondDurationIconData": Icons.remove,
+    "secondDurationIconColor": Colors.red.shade200,
+    "secondDurationSign": -1,
+    "secondDurationTextColor": Colors.red.shade200,
+    "secondDurationStr": "02:00",
+    "secondStartDateTimeStr": "2022-07-12 16:50",
+    "secondEndDateTimeStr": "2022-07-12 14:50",
+    "secondEndDateTimeCheckbox": false,
+    "thirdDurationIconData": Icons.remove,
+    "thirdDurationIconColor": Colors.red.shade200,
+    "thirdDurationSign": -1,
+    "thirdDurationTextColor": Colors.red.shade200,
+    "thirdDurationStr": "01:00",
+    "thirdStartDateTimeStr": "2022-07-12 14:50",
+    "thirdEndDateTimeStr": "2022-07-12 13:50",
+    "thirdEndDateTimeCheckbox": false,
+    "preferredDurationsItemsStr": '{"good":["12:00","3:30","10:30"]}',
+    "calcSlDurNewDateTimeStr": '14-07-2022 13:09',
+    "calcSlDurPreviousDateTimeStr": '14-07-2022 13:13',
+    "calcSlDurBeforePreviousDateTimeStr": '14-07-2022 13:12',
+    "calcSlDurCurrSleepDurationStr": '12:36',
+    "calcSlDurCurrWakeUpDurationStr": '0:02',
+    "calcSlDurCurrTotalDurationStr": '12:38',
+    "calcSlDurCurrSleepDurationPercentStr": '99.74 %',
+    "calcSlDurCurrWakeUpDurationPercentStr": '0.26 %',
+    "calcSlDurCurrTotalDurationPercentStr": '100 %',
+    "calcSlDurCurrSleepPrevDayTotalPercentStr": '79.74 %',
+    "calcSlDurCurrWakeUpPrevDayTotalPercentStr": '1.26 %',
+    "calcSlDurCurrTotalPrevDayTotalPercentStr": '81 %',
+    "calcSlDurStatus": Status.wakeUp,
+    "calcSlDurSleepTimeStrHistory": ['10-07-2022 00:58', '05:35', '04:00'],
+    "calcSlDurWakeUpTimeStrHistory": ['10-07-2022 05:58', '00:35'],
+    "alarmMedicDateTimeStr": '15-12-2022 06:00',
+    "dtDiffStartDateTimeStr": "2022-07-13 16:09",
+    "dtDiffEndDateTimeStr": "2022-07-14 16:09:42.390753",
+    "dtDiffDurationStr": "24:00",
+    "dtDiffAddTimeStr": "1:00",
+    "dtDiffFinalDurationStr": "25:00",
+    "dtDurationPercentStr": "70 %",
+    "dtDurationTotalPercentStr": "90 %",
+    "firstTimeStr": "00:10:00",
+    "secondTimeStr": "00:05:00",
+    "resultTimeStr": "00:15:00",
+    "resultPercentStr": "40 %",
+    "resultSecondPercentStr": "90 %",
+    "divideFirstBySecondCheckBox": false,
+  };
+
+  // files in this local test dir are stored in
+  // project test_data dir updated
+  // on GitHub
+  String path = kCircadianAppDataTestDir;
+  final Directory directory = Directory(path);
+  bool directoryExists = await directory.exists();
+
+  if (!directoryExists) {
+    await directory.create();
+  }
+
+  String jsonFileNameOne = '2022-07-14 13.09.json';
+  String transferDataJsonFilePathNameOne =
+      '${directory.path}${Platform.pathSeparator}$jsonFileNameOne';
+  TransferDataViewModel transferDataViewModelOne = TransferDataViewModel(
+      transferDataJsonFilePathName: transferDataJsonFilePathNameOne);
+  transferDataViewModelOne.transferDataMap = transferDataMapOne;
+  await transferDataViewModelOne.updateAndSaveTransferData();
+
+  Map<String, dynamic> transferDataMapTwo = {
+    "firstDurationIconData": Icons.add,
+    "firstDurationIconColor": Colors.green.shade200,
+    "firstDurationSign": 1,
+    "firstDurationTextColor": Colors.green.shade200,
+    "addDurStartDateTimeStr": "2022-07-12 16:00",
+    "firstDurationStr": "00:50",
+    "firstStartDateTimeStr": "2022-07-12 16:00",
+    "firstEndDateTimeStr": "2022-07-12 16:50",
+    "firstEndDateTimeCheckbox": false,
+    "secondDurationIconData": Icons.remove,
+    "secondDurationIconColor": Colors.red.shade200,
+    "secondDurationSign": -1,
+    "secondDurationTextColor": Colors.red.shade200,
+    "secondDurationStr": "02:00",
+    "secondStartDateTimeStr": "2022-07-12 16:50",
+    "secondEndDateTimeStr": "2022-07-12 14:50",
+    "secondEndDateTimeCheckbox": false,
+    "thirdDurationIconData": Icons.remove,
+    "thirdDurationIconColor": Colors.red.shade200,
+    "thirdDurationSign": -1,
+    "thirdDurationTextColor": Colors.red.shade200,
+    "thirdDurationStr": "01:00",
+    "thirdStartDateTimeStr": "2022-07-12 14:50",
+    "thirdEndDateTimeStr": "2022-07-12 13:50",
+    "thirdEndDateTimeCheckbox": false,
+    "preferredDurationsItemsStr": '{"good":["12:00","3:30","10:30"]}',
+    "calcSlDurNewDateTimeStr": '14-07-2022 16:39',
+    "calcSlDurPreviousDateTimeStr": '14-07-2022 13:13',
+    "calcSlDurBeforePreviousDateTimeStr": '14-07-2022 13:12',
+    "calcSlDurCurrSleepDurationStr": '12:36',
+    "calcSlDurCurrWakeUpDurationStr": '0:02',
+    "calcSlDurCurrTotalDurationStr": '12:38',
+    "calcSlDurCurrSleepDurationPercentStr": '99.74 %',
+    "calcSlDurCurrWakeUpDurationPercentStr": '0.26 %',
+    "calcSlDurCurrTotalDurationPercentStr": '100 %',
+    "calcSlDurCurrSleepPrevDayTotalPercentStr": '79.74 %',
+    "calcSlDurCurrWakeUpPrevDayTotalPercentStr": '1.26 %',
+    "calcSlDurCurrTotalPrevDayTotalPercentStr": '81 %',
+    "calcSlDurStatus": Status.wakeUp,
+    "calcSlDurSleepTimeStrHistory": [
+      '10-07-2022 00:58',
+      '05:35',
+      '04:00',
+      '03:00'
+    ],
+    "calcSlDurWakeUpTimeStrHistory": ['10-07-2022 05:58', '00:35', '00:30'],
+    "alarmMedicDateTimeStr": '15-12-2022 06:00',
+    "dtDiffStartDateTimeStr": "2022-07-13 16:09",
+    "dtDiffEndDateTimeStr": "2022-07-14 16:09:42.390753",
+    "dtDiffDurationStr": "24:00",
+    "dtDiffAddTimeStr": "1:00",
+    "dtDiffFinalDurationStr": "25:00",
+    "dtDurationPercentStr": "70 %",
+    "dtDurationTotalPercentStr": "90 %",
+    "firstTimeStr": "00:10:00",
+    "secondTimeStr": "00:05:00",
+    "resultTimeStr": "00:15:00",
+    "resultPercentStr": "40 %",
+    "resultSecondPercentStr": "90 %",
+    "divideFirstBySecondCheckBox": false,
+  };
+
+  String jsonFileNameTwo = '2022-07-14 16.39.json';
+  String transferDataJsonFilePathNameTwo =
+      '${directory.path}${Platform.pathSeparator}$jsonFileNameTwo';
+  TransferDataViewModel transferDataViewModelTwo = TransferDataViewModel(
+      transferDataJsonFilePathName: transferDataJsonFilePathNameTwo);
+  transferDataViewModelTwo.transferDataMap = transferDataMapTwo;
+  await transferDataViewModelTwo.updateAndSaveTransferData();
+
+  group(
+    'AppBar menu testing',
+    () {
+      testWidgets(
+        'Undo load',
+        (tester) async {
+          MainApp mainApp =
+              MainApp(transferDataViewModel: transferDataViewModelOne);
+          await tester.pumpWidget(
+            MaterialApp(
+              home: Scaffold(
+                body: mainApp,
+              ),
+            ),
+          );
+
+          // clicking on AppBar popup menu button
+          // await tester.tap(find.byKey(const Key('appBarPopupMenuButton')));
+          // await tester.pumpAndSettle();
+
+          // // then clicking on the Load menu item
+          // await tester.tap(find.byKey(const Key('appBarMenuLoad')).first);
+          // await tester.pumpAndSettle();
+
+          // // then loading jsonFileNameOne
+          // await tester.tap(find.text(jsonFileNameOne).first);
+          // await tester.pumpAndSettle();
+
+          // // re-clicking on AppBar popup menu button.
+          await tester.tap(find.byKey(const Key('appBarPopupMenuButton')));
+          await tester.pumpAndSettle();
+
+          // then clicking on the Load menu item
+          await tester.tap(find.byKey(const Key('appBarMenuLoad')).first);
+          await tester.pumpAndSettle();
+
+          // then loading jsonFileNameTwo
+          await tester.tap(find.text(jsonFileNameTwo).first);
+          await tester.pumpAndSettle();
+
+          int a = 1;
+        },
+      );
+    },
+  );
+}
