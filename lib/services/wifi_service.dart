6aed2a5ad98896ce8a4e77fa17d475f41a4059ff
Archanciel <jp.schnyder@gmail.com>
2023-09-15 19:25:30 +0200

    copied file from fixed_start ... parent branch

--- lib/services/wifi_service.dart
+++ lib/services/wifi_service.dart
@@ -2,7 +2,16 @@ import 'package:connectivity/connectivity.dart';
 
 class WifiService {
   static Future<bool> isWifiEnabled() async {
-    var connectivityResult = await (Connectivity().checkConnectivity());
+    ConnectivityResult connectivityResult;
+
+    try {
+      connectivityResult = await (Connectivity().checkConnectivity());
+    } catch (_) { // catching any exeption type
+      // the case when running the app on Windows where
+      // Connectivity().checkConnectivity() is not supported.
+      return false;
+    }
+
     if (connectivityResult == ConnectivityResult.wifi) {
       return true;
     } else {
