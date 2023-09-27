3a2f4d8c36f4d4500a464f5bf34887a63c886e54
Archanciel <jp.schnyder@gmail.com>
2023-09-27 18:03:03 +0200

    removed unused code. tst ok.

--- integration_test/main_app_integration_test.dart
+++ integration_test/main_app_integration_test.dart
@@ -407,85 +407,6 @@ Future<void> main() async {
                       transferDataJsonFilePathNameOne);
           transferDataViewModelOne.transferDataMap = transferDataMapCircadian;
           await transferDataViewModelOne.updateAndSaveTransferData();
-
-          Map<String, dynamic> transferDataMapTwo = {
-            "firstDurationIconData": Icons.add,
-            "firstDurationIconColor": Colors.green.shade200,
-            "firstDurationSign": 1,
-            "firstDurationTextColor": Colors.green.shade200,
-            "addDurStartDateTimeStr": "2022-07-12 16:00",
-            "firstDurationStr": "00:50",
-            "firstStartDateTimeStr": "2022-07-12 16:00",
-            "firstEndDateTimeStr": "2022-07-12 16:50",
-            "firstEndDateTimeCheckbox": false,
-            "secondDurationIconData": Icons.remove,
-            "secondDurationIconColor": Colors.red.shade200,
-            "secondDurationSign": -1,
-            "secondDurationTextColor": Colors.red.shade200,
-            "secondDurationStr": "02:00",
-            "secondStartDateTimeStr": "2022-07-12 16:50",
-            "secondEndDateTimeStr": "2022-07-12 14:50",
-            "secondEndDateTimeCheckbox": false,
-            "thirdDurationIconData": Icons.remove,
-            "thirdDurationIconColor": Colors.red.shade200,
-            "thirdDurationSign": -1,
-            "thirdDurationTextColor": Colors.red.shade200,
-            "thirdDurationStr": "01:00",
-            "thirdStartDateTimeStr": "2022-07-12 14:50",
-            "thirdEndDateTimeStr": "2022-07-12 13:50",
-            "thirdEndDateTimeCheckbox": false,
-            "preferredDurationsItemsStr":
-                '{"good":["12:00","3:30","10:30","false","true"]}',
-            "calcSlDurNewDateTimeStr": '14-07-2022 16:39',
-            "calcSlDurPreviousDateTimeStr": '14-07-2022 13:13',
-            "calcSlDurBeforePreviousDateTimeStr": '14-07-2022 13:12',
-            "calcSlDurCurrSleepDurationStr": '12:36',
-            "calcSlDurCurrWakeUpDurationStr": '0:02',
-            "calcSlDurCurrTotalDurationStr": '12:38',
-            "calcSlDurCurrSleepDurationPercentStr": '99.74 %',
-            "calcSlDurCurrWakeUpDurationPercentStr": '0.26 %',
-            "calcSlDurCurrTotalDurationPercentStr": '100 %',
-            "calcSlDurCurrSleepPrevDayTotalPercentStr": '79.74 %',
-            "calcSlDurCurrWakeUpPrevDayTotalPercentStr": '1.26 %',
-            "calcSlDurCurrTotalPrevDayTotalPercentStr": '81 %',
-            "calcSlDurStatus": Status.wakeUp,
-            "calcSlDurSleepTimeStrHistory": [
-              '10-07-2022 00:58',
-              '05:35',
-              '04:00',
-              '03:00'
-            ],
-            "calcSlDurWakeUpTimeStrHistory": [
-              '10-07-2022 05:58',
-              '00:35',
-              '00:30'
-            ],
-            "alarmMedicDateTimeStr": '15-12-2022 06:00',
-            "dtDiffStartDateTimeStr": "2022-07-13 16:09",
-            "dtDiffEndDateTimeStr": "2022-07-14 16:09:42.390753",
-            "dtDiffDurationStr": "24:00",
-            "dtDiffAddTimeStr": "1:00",
-            "dtDiffFinalDurationStr": "25:00",
-            "dtDurationPercentStr": "70 %",
-            "dtDurationTotalPercentStr": "90 %",
-            "firstTimeStr": "00:10:00",
-            "secondTimeStr": "00:05:00",
-            "resultTimeStr": "00:15:00",
-            "resultPercentStr": "40 %",
-            "resultSecondPercentStr": "90 %",
-            "divideFirstBySecondCheckBox": false,
-          };
-
-          String jsonFileNameTwo = '2022-07-14 16.39.json';
-          String transferDataJsonFilePathNameTwo =
-              '$testPath${Platform.pathSeparator}$jsonFileNameTwo';
-          TransferDataViewModel transferDataViewModelTwo =
-              TransferDataViewModel(
-                  transferDataJsonFilePathName:
-                      transferDataJsonFilePathNameTwo);
-          transferDataViewModelTwo.transferDataMap = transferDataMapTwo;
-          await transferDataViewModelTwo.updateAndSaveTransferData();
-
           await tester.pumpWidget(
             MaterialApp(
               home: Scaffold(
@@ -2026,6 +1947,8 @@ Future<void> main() async {
           transferDataViewModelCircadian.transferDataMap =
               transferDataMapCircadian;
           await transferDataViewModelCircadian.updateAndSaveTransferData();
+
+          // starting the app with the circadian.json file
 
           await tester.pumpWidget(
             MaterialApp(
